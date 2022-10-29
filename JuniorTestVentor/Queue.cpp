6dfff609a3e8b3bf30fa1d037eb112bf3560b69a
vislovalexey111 <vislovalexey111@gmail.com>
2022-10-29 23:42:52 +0700

    Finishing up
    - Finishing redone from linear queue to circular!

--- JuniorTestVentor/Queue.cpp
+++ JuniorTestVentor/Queue.cpp
@@ -5,17 +5,17 @@ const uint16 Min(const uint16 a, const uint16 b)
 	return (a < b) ? a : b;
 }
 
-Queue::Queue(uint16 iSize) : hData(NULL), Head(0), Tail(iSize), Size(iSize)
+Queue::Queue(uint16 iSize) : hData(NULL), Head(0), Size(iSize)
 {
 	// We are making empty queue with allocated memory of "Size", not the queue with data, that have the size = "Size"
 	// In this case "Size" is just a maximum amount of data that we can pass to the queue. Because the Tail is unsigned int,
-	// it is better to make it point to index AFTER last element and refer to last element as array[Tail - 1].
+	// it is better to make Tail index equal to initial Size, so we can understand that queue data is empty. 
 	if(Size > 0) 
 		hData = (uint8*)calloc(Size, sizeof(uint8));
 
 	// In case of memory allocation error, we setting the size to 0
-	if (!hData)
-		Size = 0;
+	Size = (!hData && Size > 0) ? 0 : Size;
+	Tail = Size;
 }
 
 Queue::~Queue(void)
@@ -29,25 +29,26 @@ Queue::~Queue(void)
 
 uint16 Queue::Put(uint8* Data, uint16 iSize)
 {
-	uint16 actualSize = (Tail < Head) ? (Size - Head + Tail + 2) : (Tail - Head);
 	// If we do not have enugh space to put all the data - we do not put the data at all. 
-	if (!hData || !Data || (actualSize > Size && Tail != Size))
+	if (!hData || !Data || !iSize || this->GetSize() + iSize > Size)
 		return 0;
-	
 
+	uint16 remainingData = iSize - Head - (Tail == Size);
+	Tail = (Tail == Size) ? Head : Tail;
 
-	
+	if (Tail < Head || Head >= iSize) 
+	{
+		Head -= iSize;
+		memcpy(&hData[Head], Data, iSize);
+	}
+	else 
+	{
+		memcpy(hData, Data + remainingData, iSize - remainingData);
+		Head = Size - remainingData;
+		memcpy(&hData[Head], Data, remainingData);
+	}
 
-
-
-	
-	
-	memcpy(hData + iSize, hData, Tail);
-	Tail += iSize;
-
-	memcpy(hData, Data, actualSize);
-	
-	return actualSize;
+	return iSize;
 }
 
 uint16 Queue::GetSize(void)
@@ -55,32 +56,30 @@ uint16 Queue::GetSize(void)
 	if (!hData || Tail == Size)
 		return 0;
 
-	return Tail;
+	return ((Tail < Head) ? (Size - Head + Tail) : (Tail - Head)) + 1;
 }
 
 uint16 Queue::Get(uint8* Data, uint16 iSize)
 {
 	if (!Data || !hData || !iSize || Tail == Size)
 		return 0;
-
-	uint16 actualSize = 1;
-	uint16 remainingData = 0;
+	
+	uint16 transferSize = 0;
 
 	if (Tail < Head) {
-		actualSize += iSize - Head;
-		if (Head + iSize > Size)
+		transferSize = Min(iSize, Size - Head);
+		
+		if (transferSize < iSize)
 		{
-			remainingData = Min((Head + iSize) % Size, Tail + 1);
-			memcpy(Data + actualSize, &hData, remainingData);
+			uint16 remainingSize = Min(iSize - transferSize, Tail + 1);
+			memcpy(Data + transferSize, hData, remainingSize);
 		}
 	}
 	else
-		actualSize += Tail - Head;
+		transferSize = Min(this->GetSize(), iSize);
 
-	memcpy(Data, &hData[Head], actualSize);
-	actualSize += remainingData;
-
-	return actualSize;
+	memcpy(Data, &hData[Head], transferSize);
+	return Min(this->GetSize(), iSize);
 }
 
 uint16 Queue::Clear(uint16 iSize)
@@ -88,8 +87,14 @@ uint16 Queue::Clear(uint16 iSize)
 	if (!hData || !iSize|| Tail == Size)
 		return 0;
 	
-	uint16 actualSize = 1;
-	
+	const uint16 initialSize = this->GetSize();
 
-	return actualSize;
+	if (iSize >= initialSize)
+		Tail = Size;
+	else if (Tail < Head)
+		Tail = (iSize > Tail) ? ((Size + Tail) - iSize) : (Tail - iSize);
+	else
+		Tail -= iSize;
+
+	return initialSize - this->GetSize();
 }
--- JuniorTestVentor/Source.cpp
+++ JuniorTestVentor/Source.cpp
@@ -1,39 +1,32 @@
-#include <cstdio>
 #include "Queue.h"
 
 void PrintArray(const char* dataArrayName, const uint8* dataArray, const uint16 dataArraySize);
-void PrintQueue(Queue& queue, const uint16 queueSize);
 void FillArrayWithNumbers(uint8* dataArray, const uint16 dataArraySize, const int step);
 
 // Testing implemented Queue data structure
 int main()
 {
 	Queue queue(8);
-	
 	uint16 smallDataSize = 4;
-	uint16 bigDataSize = smallDataSize * 3;
 	uint8* smallData = (uint8*)calloc(smallDataSize, sizeof(uint8));
 
 	if (!smallData)
 		return 0;
 
+	uint16 bigDataSize = smallDataSize * 3;
 	uint8* bigData = (uint8*)calloc(bigDataSize, sizeof(uint8));
 	
 	if (!bigData)
 		return 0;
-
+	
 	FillArrayWithNumbers(smallData, smallDataSize, 1);
 	FillArrayWithNumbers(bigData, bigDataSize, 2);
 
 	// Trying to put big amount of data.
 	printf("Data bytes transfered from big array: %hu\n", queue.Put(bigData, bigDataSize));
-	printf("Size of Queue: %hu\n", queue.GetSize());
-
+	
 	// Putting small amount of data
 	printf("\nData bytes transfered from small array: %hu\n", queue.Put(smallData, smallDataSize));
-	printf("Size of Queue: %hu\n\n", queue.GetSize());
-
-	PrintQueue(queue, queue.GetSize());
 
 	// Transfering data frome queue to Big data array and printing results
 	printf("\nTransfered bytes from queue to big data array: %hhu\n", queue.Get(bigData, bigDataSize));
@@ -46,11 +39,21 @@ int main()
 
 	// Clearing last 2 elements from the queue
 	queue.Clear(2);
-	PrintQueue(queue, queue.GetSize());
+	printf("after clearing 2 elements\n");
 
 	// Putting new "smallData" elements to the head of the queue
 	queue.Put(smallData, smallDataSize);
-	PrintQueue(queue, queue.GetSize());
+	
+	queue.Clear(2);
+	printf("after clearing 2 elements\n");
+
+	queue.Put(smallData, smallDataSize);
+
+	queue.Clear(2);
+	printf("after clearing 2 elements\n");
+
+	queue.Clear(3);
+	printf("after clearing 2 elements\n");
 
 	// Releasing memory we no longer need
 	free(bigData);
@@ -61,23 +64,6 @@ int main()
 	getchar();
 	return 0;
 }	
-
-
-void PrintQueue(Queue& queue, const uint16 queueSize)
-{
-	// Creating another array and testing queue.Get function to store current quque data inside new array.
-	uint8* queueData = (uint8*)calloc(queueSize, sizeof(uint8));
-
-	if (!queueData)
-		return;
-
-	queue.Get(queueData, queueSize);
-	PrintArray("Queue", queueData, queueSize);
-
-	// Releasing memory we no longer need
-	free(queueData);
-	queueData = NULL;
-}
 
 void PrintArray(const char* dataArrayName, const uint8* dataArray, const uint16 dataArraySize)
 {
